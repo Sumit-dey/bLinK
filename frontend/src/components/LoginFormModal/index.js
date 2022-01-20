00d9442ce2a32b3cbbee0d13f90ba1214a19f1a3
Sumit-dey <deysumit417@gmail.com>
2022-01-20 18:26:21 -0500

    aa

---
+++ frontend/src/components/LoginFormModal/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Modal } from "../../context/Modal";
+import LoginForm from "./LoginForm";
+import "./LoginFormModal.css";
+
+function LoginFormModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button className="loginBtn" onClick={() => setShowModal(true)}>
+        Log In
+      </button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <LoginForm />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default LoginFormModal;
