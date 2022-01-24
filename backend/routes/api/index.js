8181dee5417740e9677f707cd30b964ba9396feb
Sumit-dey <deysumit417@gmail.com>
2022-01-24 15:24:23 -0500

    last push

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,13 +8,14 @@ const usersRouter = require('./users.js');
 const imageRouter = require('./images.js');
 const profileRouter = require('./profile.js');
 const albumRouter = require('./albums');
+const commentRouter = require('./comments.js');
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/images', imageRouter);
 router.use('/profile', profileRouter);
-router.use('/albums', albumRouter)
-
+router.use('/albums', albumRouter);
+router.use('/comments', commentRouter);
 
 router.post('/test', (req, res) => {
     res.json({
