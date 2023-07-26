e02608f1e2b8bb97cd01abf028a9558fa165b0e9
Sumit-dey <deysumit417@gmail.com>
2023-07-25 23:08:04 -0400

    aa

--- backend/config/database.js
+++ backend/config/database.js
@@ -8,12 +8,23 @@ const host = db.host;
 
 module.exports = {
   development: {
-    username,
-    password,
-    database,
-    host,
+    // username,
+    // password,
+    // database,
+    // host,
+    // DATABASE_URL: "postgresql://jarif:jdiY4nc8P0vzoMjTwU6J8g@fairbndb-8109.7tt.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
+    // dialect: 'postgres',
+    // seederStorage: 'sequelize',
+
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    seederStorage: 'sequelize'
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     use_env_variable: 'DATABASE_URL',
@@ -21,7 +32,7 @@ module.exports = {
     define: {
       schema: process.env.SCHEMA
     },
-    
+
     seederStorage: 'sequelize',
     dialectOptions: {
       ssl: {
